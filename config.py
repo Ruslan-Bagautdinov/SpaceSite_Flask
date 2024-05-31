bbc611014df74f91bc57a08d1718e8fcb8e74d69
ruslan3lodey <ruslan3lodey@gmail.com>
2024-05-31 10:37:15 +0300

    v. 1.0.0 docker 100% with MySQL DB and vulnerabilities fixes. On GitHub. default values in config fix

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = getenv('SECRET_KEY', 'secret-key')
 
 ACCESS_TOKEN_EXPIRE_MINUTES = int(getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 15))
 REFRESH_TOKEN_EXPIRE_MINUTES = int(getenv('REFRESH_TOKEN_EXPIRE_MINUTES', 10080))
 
-MYSQL_HOST = getenv('MYSQL_HOST')
-MYSQL_PORT = getenv('MYSQL_PORT')
-MYSQL_USER = getenv('MYSQL_USER')
-MYSQL_PASSWORD = getenv('MYSQL_PASSWORD')
-MYSQL_ROOT_PASSWORD = getenv('MYSQL_ROOT_PASSWORD')
-MYSQL_DATABASE = getenv('MYSQL_DATABASE')
+MYSQL_HOST = getenv('MYSQL_HOST', 'localhost')
+MYSQL_PORT = getenv('MYSQL_PORT', '3306')
+MYSQL_USER = getenv('MYSQL_USER', 'user')
+MYSQL_PASSWORD = getenv('MYSQL_PASSWORD', 'password')
+MYSQL_ROOT_PASSWORD = getenv('MYSQL_ROOT_PASSWORD', 'password')
+MYSQL_DATABASE = getenv('MYSQL_DATABASE', 'database')
 
 DATABASE_URL = (f"mysql+pymysql"
                 f"://{MYSQL_USER}"
@@ -22,10 +22,8 @@ DATABASE_URL = (f"mysql+pymysql"
                 f"@{MYSQL_HOST}"
                 f"/{MYSQL_DATABASE}")
 
-UNSPLASH_ACCESS_KEY = getenv('UNSPLASH_ACCESS_KEY')
+UNSPLASH_ACCESS_KEY = getenv('UNSPLASH_ACCESS_KEY', 'unsplash-access-key')
 
 BASE_DIR = path.abspath(path.dirname(__file__))
 IMAGE_DIR = path.join(BASE_DIR, 'photo')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-
